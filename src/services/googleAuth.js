91d0d9ce721fcf363e9b4a88b8ac1a563f95f984
DoliTim <timdolinsek88@gmail.com>
2024-10-24 15:33:51 +0200

    Fix jwt-decode import and update dependency

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -1,10 +1,1 @@
-import jwt_decode from 'jwt-decode';
-
-export const handleGoogleSignIn = (response) => {
-  const userObject = jwt_decode(response.credential);
-  return {
-    name: userObject.name,
-    email: userObject.email,
-    picture: userObject.picture
-  };
-};
+ 
