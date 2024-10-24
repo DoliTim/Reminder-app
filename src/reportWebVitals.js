5a184e16efff43860b07e42f1f671e001e3fa8df
DoliTim <timdolinsek88@gmail.com>
2024-10-24 14:06:38 +0200

    Initial commit

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// src/reportWebVitals.js
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(
+        ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+          getCLS(onPerfEntry);
+          getFID(onPerfEntry);
+          getFCP(onPerfEntry);
+          getLCP(onPerfEntry);
+          getTTFB(onPerfEntry);
+        }
+      );
+    }
+  };
+  
+  export default reportWebVitals;
+  
