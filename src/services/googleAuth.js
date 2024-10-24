e27df7aec8df3a2bae245045efc2fab79d74e910
DoliTim <timdolinsek88@gmail.com>
2024-10-24 15:20:28 +0200

    Fix useEffect dependency and add Babel plugin

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -1,1 +1,10 @@
- 
+import jwt_decode from 'jwt-decode';
+
+export const handleGoogleSignIn = (response) => {
+  const userObject = jwt_decode(response.credential);
+  return {
+    name: userObject.name,
+    email: userObject.email,
+    picture: userObject.picture
+  };
+};
--- src/services/makeService.js
+++ src/services/makeService.js
@@ -1,1 +1,30 @@
- 
+import axios from 'axios';
+
+const MAKE_WEBHOOK_URL = process.env.REACT_APP_MAKE_WEBHOOK_URL;
+
+export const sendUserDataToMake = async (userData) => {
+  try {
+    await axios.post(`${MAKE_WEBHOOK_URL}/user`, userData);
+  } catch (error) {
+    console.error('Error sending user data to Make:', error);
+  }
+};
+
+export const addReminderToMake = async (task, userEmail) => {
+  try {
+    const response = await axios.post(`${MAKE_WEBHOOK_URL}/reminder`, { task, userEmail });
+    return response.data;
+  } catch (error) {
+    console.error('Error adding reminder to Make:', error);
+  }
+};
+
+export const getRemindersFromMake = async (userEmail) => {
+  try {
+    const response = await axios.get(`${MAKE_WEBHOOK_URL}/reminders?email=${userEmail}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching reminders from Make:', error);
+    return [];
+  }
+};
